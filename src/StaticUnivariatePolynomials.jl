134f2110b7bd978f2c769f71276773d94455378f
Twan Koolen <koolen.twan@gmail.com>
2019-01-08 16:56:37 -0500

    Basics

--- src/StaticUnivariatePolynomials.jl
+++ src/StaticUnivariatePolynomials.jl
@@ -1,5 +1,47 @@
 module StaticUnivariatePolynomials
 
-greet() = print("Hello World!")
+export
+    Polynomial
+
+struct Polynomial{N, T}
+    coeffs::NTuple{N, T}
+end
+
+Polynomial(coeffs::Tuple) = Polynomial(promote(coeffs...))
+Polynomial(coeffs...) = Polynomial(coeffs)
+
+@inline function Polynomial{N}(p::Polynomial{M, T}) where {N, M, T}
+    N < M && throw(InexactError(:Polynomial, Polynomial{N}, p))
+    Polynomial((p.coeffs..., ntuple(_ -> zero(T), Val(N - M))...))
+end
+
+# Evaluation
+(p::Polynomial{1})(x::Number) = p.coeffs[1] # evalpoly doesn't handle N = 1 case
+@generated function (p::Polynomial{N})(x::Number) where N
+    quote
+        coeffs = p.coeffs
+        @evalpoly(x, $((:(p.coeffs[$i]) for i = 1 : N)...))
+    end
+end
+
+# Arithmetic
+for op in [:+, :-]
+    @eval begin
+        function Base.$op(p1::Polynomial{N}, p2::Polynomial{N}) where N
+            c1 = p1.coeffs
+            c2 = p2.coeffs
+            Polynomial(ntuple(i -> $op(c1[i], c2[i]), Val(N)))
+        end
+        function Base.$op(p1::Polynomial{N}, p2::Polynomial{M}) where {N, M}
+            P = max(N, M)
+            $op(Polynomial{P}(p1), Polynomial{P}(p2))
+        end
+    end
+end
+
+derivative(p::Polynomial{1}) = Polynomial(zero(p.coeffs[1]))
+# @generated function derivative(p::Polynomial{N}) where N
+
+# end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,26 @@
 using StaticUnivariatePolynomials
 using Test
 
-@testset "StaticUnivariatePolynomials.jl" begin
-    # Write your own tests here.
+@testset "evaluation" begin
+    @test Polynomial(1)(2) == 1
+    @test Polynomial(2, 3, 4)(5) == 2 + 3 * 5 + 4 * 5^2
+end
+
+@testset "padding" begin
+    p = Polynomial(2, 3, 4)
+    q = Polynomial{20}(p)
+    @test p(5) == q(5)
+    allocs = @allocated Polynomial{20}(p)
+    @test allocs == 0
+end
+
+@testset "arithmetic" begin
+    p1 = Polynomial(2, 3, 4)
+    p2 = Polynomial(6, 7, 8)
+    @test (p1 + p2)(7) == p1(7) + p2(7)
+    allocs = @allocated p1 + p2
+    @test allocs == 0
+
+    p3 = Polynomial(ntuple(_ -> rand(), Val(20)))
+    @test (p1 + p3)(7) == p1(7) + p3(7)
 end
